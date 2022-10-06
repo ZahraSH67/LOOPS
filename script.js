ca8a1db92a9ffc6c7ff21862b107221f2a124cb3
Zahra <zahra.shahabi.dk@gmail.com>
2022-10-06 13:24:47 +0200

    LOOPS

---
+++ script.js
@@ -0,0 +1,15 @@
+
+function evenNumbers(num1, num2){
+    if(num2 > num1){
+        for(let i = num1 + 1; i < num2; i++){
+            console.log(i)
+        }
+    }else{
+        for (let i = num2 + 1; i < num1; i++){
+            console.log(i)
+        }
+    }
+
+    }
+// console.log(evenNumbers(2, 19))
+
